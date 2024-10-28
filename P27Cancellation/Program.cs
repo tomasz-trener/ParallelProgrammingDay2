648f3e43ab93007bde66e20f63eb255e7e2f2d2f
Tomasz Leś <tomasz.les@pw.edu.pl>
2024-10-28 11:16:32 +0100

    q

--- P27Cancellation/Program.cs
+++ P27Cancellation/Program.cs
@@ -1,2 +1,117 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+public class Example1
+{
+
+
+    static void Main1()
+    {
+
+        var numbers = Enumerable.Range(1, 100).ToArray();
+
+        var cts = new CancellationTokenSource();
+
+        var results = numbers.AsParallel()
+            .WithMergeOptions(ParallelMergeOptions.NotBuffered)
+            .WithCancellation(cts.Token)
+            .Select(x =>
+            {
+                if(cts.Token.IsCancellationRequested)
+                {
+                    //  Console.WriteLine("Cancellation requested");
+                    //  cts.Token.ThrowIfCancellationRequested();
+                    return 0;
+                }
+
+
+
+                var result = Math.Pow(x, 2);
+                Thread.Sleep(1000);
+                return result;
+            });
+
+
+        try
+        {
+            foreach (var item in results)
+            {
+                if (item > 50)
+                    cts.Cancel();
+
+                Console.WriteLine($"{item} C");
+            }
+
+        }
+        catch (OperationCanceledException)
+        {
+            Console.WriteLine("cancelled");
+        }
+        
+
+
+
+    }
+}
+
+
+
+public class Example2
+{
+
+
+    static void Main()
+    {
+
+        var numbers = Enumerable.Range(1, 100).ToArray();
+
+        var cts = new CancellationTokenSource();
+
+        var results = numbers.AsParallel()
+            .WithMergeOptions(ParallelMergeOptions.NotBuffered)
+            .WithCancellation(cts.Token)
+            .Select(x =>
+            {
+                if (cts.Token.IsCancellationRequested)
+                {
+                    //  Console.WriteLine("Cancellation requested");
+                    //  cts.Token.ThrowIfCancellationRequested();
+                    return 0;
+                }
+
+                if (x == 50)
+                    throw new Exception("50 is not allowed");
+
+                var result = Math.Pow(x, 2);
+                Thread.Sleep(1000);
+                return result;
+            });
+
+
+        try
+        {
+            foreach (var item in results)
+            {
+               // if (item > 50)
+                //    cts.Cancel();
+
+                Console.WriteLine($"{item} C");
+            }
+
+        }
+        catch (OperationCanceledException)
+        {
+            Console.WriteLine("cancelled");
+        }
+        catch(AggregateException ex)
+        {
+            ex.Handle(e =>
+            {
+                Console.WriteLine("error");
+                return true;
+            });
+        }
+
+
+
+    }
+}
+
---
+++ P28CustomAggregations/Program.cs
@@ -0,0 +1,34 @@
+﻿public class Example1
+{
+
+
+    static void Main()
+    {
+
+       var sum=   Enumerable.Range(1, 1000).Sum();
+        Console.WriteLine(sum);
+
+
+        var sum1= Enumerable.Range(1, 1000)
+            .Aggregate(
+                0,
+                (acc, n) => acc + n,
+                acc => acc
+            );
+        Console.WriteLine(sum1);
+
+
+       var sum2= ParallelEnumerable.Range(1, 1000)
+            .Aggregate(
+                0,
+                (partialSum, n) => partialSum + n,
+                (total, subTotal) => total + subTotal,
+                acc => acc
+            );
+
+
+        Console.WriteLine(sum2);
+
+    }
+}
+
